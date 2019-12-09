94316ba6749b152e0ad0f21a6888707796bb02e5
Hang Zhou <zhou_hang@berkeley.edu>
2019-12-08 16:27:47 -0800

    loading the dataset

--- src/tools/dataset.py
+++ src/tools/dataset.py
@@ -1,26 +1,25 @@
 import numpy as np
 import pickle
 import sys, os
+from tools.utils import *
 
 DATASET_EXT = "batch"
+default_path = default_dataset_path()
 
-def get_files_with_extension(directory, extension):
-    files = []
-    for name in os.listdir(directory):
-        if name.endswith(extension):
-            files.append(f'{directory}/{name}')
-    return files
-
-
-def extract_batches(path):
+def extract_batches(path = default_path):
     batch_names = get_files_with_extension(path, DATASET_EXT)
     for name in batch_names:
-        data, label = extract_single_batch(path, name)
+        data, label = extract_single_batch(name)
+        print(data[:10])
+        print(label[:10])
 
-def extract_single_batch(path, name):
-    with open(path+name, "rb") as f:
+def extract_single_batch(name):
+    with open(name, "rb") as f:
         batch = pickle.load(f)
     data = batch['data']
     label = batch['label']
     return data, label
 
+
+if __name__ == "__main__":
+    extract_batches()
---
+++ src/tools/utils.py
@@ -0,0 +1,44 @@
+"""
+A little notes on python path-related lib:
+    os.listdir(path) 
+        can accept the relative path
+    pathlib         
+        This module offers classes representing filesystem paths with semantics appropriate for different operating systems.
+    open()
+        can accept the relative path
+"""
+import os
+import pathlib
+
+"""
+default project/src/tools/utils.py dirctory
+"""
+project_path = pathlib.Path(__file__).parent.parent.parent
+
+def default_dataset_path():
+    "get the default path of our dataset"
+    return project_path / "dataset"
+ 
+def exam_path(dirctory):
+    "check if a path is valid in the system"
+    pass
+
+def get_files_with_extension(directory, extension):
+    """
+    The directory here can't solve 
+    """
+    files = []
+    for name in os.listdir(directory):
+        if name.endswith(extension):
+            files.append(f'{directory}/{name}')
+    return files
+
+   
+def check_dataset():
+    "check if the none-default dataset provided is valid"
+    pass
+
+
+if __name__ == "__main__":
+    files = get_files_with_extension(default_dataset_path(), "batch")
+    print(files[:10])
