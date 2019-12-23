ca96bb388e0a9347f7a7c31576fa97f7e6a45765
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-23 07:38:25 +0000

    complete summary

---
+++ src/models/report_models.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tools.utils import *
+from tools.dataset import *
+from models.toy_model import new_ds
+
+checkpoint_dir = lambda idx: str(default_train_sum_path()) + "/checkpoint_test/model_{}/".format(idx)
+tensorboard_path = lambda idx: str(default_train_sum_path()) + "/summary_test/model_{}/".format(idx)
+
+checkpoint_train_path = "train-001.hdf5"
+checkpoint_valid_path = "valid-001.hdf5"
+
+model_saved_path = str(default_train_sum_path()) + "/models_test/"
+model_saved_name = lambda idx: model_saved_path + "model_{}.h5".format(idx)
+
+def restore(model_path, checkpoints_path):
+    model = tf.keras.models.load_model(model_path)
+    print(model.summary())
+    #latest = tf.train.latest_checkpoint(checkpoints_path)
+    model.load_weights(checkpoints_path)
+    return model
+
+def evaluate(model):
+    pass
+
+
+if __name__ == "__main__":
+    model_1 = restore(model_saved_name(1), checkpoint_dir(1)+checkpoint_valid_path)
+    model_2 = restore(model_saved_name(2), checkpoint_dir(2)+checkpoint_train_path)
+    valid = new_ds(False)
+    loss, acc = model_1.evaluate(valid, verbose = 2)
+    print("Restored model, accuracy: {:5.2f}%".format(100*acc))
--- src/models/toy_model.py
+++ src/models/toy_model.py
@@ -13,9 +13,16 @@ checkpoint_dir = lambda idx: str(default_train_sum_path()) + "/checkpoint_test/m
 checkpoint_path = "cp-{epoch:04d}.ckpt"
 tensorboard_path = lambda idx: str(default_train_sum_path()) + "/summary_test/model_{}/".format(idx)
 
-checkpoint_train_path = "train_weights.{epoch:03d}.hdf5"
-checkpoint_valid_path = "valid_weights.{epoch:03d}.hdf5" #-{val_acc:.4f}
+checkpoint_train_path = "train-{epoch:03d}.hdf5"
+checkpoint_valid_path = "valid-{epoch:03d}.hdf5" #-{val_acc:.4f}
 
+model_saved_path = str(default_train_sum_path()) + "/models_test/"
+model_saved_name = lambda idx: model_saved_path + "model_{}.h5".format(idx)
+
+
+###############
+# Model definition
+###############
 def toy_net(layers, batch_size = 1):
     input_dims = 2*BITS + OPS_BITS
     output_dims = 1
@@ -26,6 +33,22 @@ def toy_net(layers, batch_size = 1):
     model.add(keras.layers.Dense(units=output_dims, activation='relu'))
     return model
 
+
+#################
+# Dataset importint
+################
+def new_ds(train = True):
+    if train:
+        dataset = input_fn(str(default_path / "alu_6_train.csv"), 16, [True for i in range(1)] + [False for i in range(5)])
+        dataset = dataset.repeat()
+    else:
+        dataset = input_fn(str(default_path / "alu_6_valid.csv"), 16, [True for i in range(1)] + [False for i in range(5)])
+    return dataset
+
+
+##############
+# Training definition
+##############
 # @tf.function
 def train(model, train_set, valid_set, callbacks):
     """
@@ -39,7 +62,7 @@ def train(model, train_set, valid_set, callbacks):
         history = model.fit(
             train_set, 
             steps_per_epoch = 70, #600
-            epochs=3, # 20
+            epochs=2, # 20
             callbacks = callbacks,
             validation_data=valid_set, 
             validation_steps=None,
@@ -53,21 +76,6 @@ def train(model, train_set, valid_set, callbacks):
             callbacks = callbacks,
         )
  
-    # for x,y in dataset:
-    #     with tf.GradientTape() as tape:
-    #         prediction=model(x)
-    #         loss = loss_fn(prediction, y)
-    #     gradients = tape.gradient(loss, model.trainable_variable)
-    #     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-def new_ds(train = True):
-    if train:
-        dataset = input_fn(str(default_path / "alu_6_train.csv"), 16, [True for i in range(1)] + [False for i in range(5)])
-        dataset = dataset.repeat()
-    else:
-        dataset = input_fn(str(default_path / "alu_6_valid.csv"), 16, [True for i in range(1)] + [False for i in range(5)])
-    return dataset
-
 cp_callback_no_valid = lambda idx : tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_dir(idx)+checkpoint_path,
                                                  monitor = "acc",
                                                  verbose=1,
@@ -109,23 +117,27 @@ tb_callback_test = lambda idx : tf.keras.callbacks.TensorBoard(log_dir = tensorb
         update_freq = 10000)
 
 
-
-
-layers = [55, 50, 40, 35, 30, 25, 20, 15]
+#############
+# Training Entry
+############
 import os
-def train_on(layers,input_dim, output_dim, train_set, valid_set, callbacks_fn, idx):
+def train_on(layers, input_dim, output_dim, train_set, valid_set, callbacks_fn, idx):
     model = toy_net(layers)
     print("capacity of this dense net: {}".format(cap_estimate(layers,input_dim, output_dim)))
     if not os.path.exists(checkpoint_dir(idx)):
         os.makedirs(checkpoint_dir(idx))
     if not os.path.exists(tensorboard_path(idx)):
         os.makedirs(tensorboard_path(idx))
+    if not os.path.exists(model_saved_path):
+        os.makedirs(model_saved_path)
     print("training on model:{}".format(idx))
     print(model.summary())
     callbacks = [cb_fn(idx) for cb_fn in callbacks_fn]
     train(model, train_set, valid_set, callbacks)
+    model.save(model_saved_name(idx))
 
 if __name__ == "__main__":
-    callbacks = [cp_callback_train_test, cp_callback_valid_test, tb_callback_test]
+    layers = [55, 50, 40, 35, 30, 25, 20, 15]
+    callbacks = [cp_callback_train_test,cp_callback_valid_test, tb_callback_test]
     train_on(layers, 16, 1, new_ds(True), new_ds(False), callbacks, 1)
     train_on(layers, 16, 1, new_ds(True), new_ds(False), callbacks, 2)
